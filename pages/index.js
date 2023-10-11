62a39946ba0485ef56eca9eda842f827edaefeba
Nihad <61694826+kardespro@users.noreply.github.com>
2023-10-11 10:48:28 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,26 +15,26 @@ export default function Home({ discord_data, articles }) {
   return (
     <>
       <Head>
-        <title>{`bencan.net: Anasayfa`}</title>
+        <title>{`nego.rocks: Anasayfa`}</title>
         <meta name="description" content="Benim kişisel web sitem." />
-        <meta name="keywords" content="bencan, bencan.net, bencan.net, bencan.net, bencan.net" />
-        <meta name="author" content="Discord @ben.can" />
+        <meta name="keywords" content="bencan, bencan.net, bencan.net, bencan.net, bencan.net, nego.rocks, chimp gey" />
+        <meta name="author" content="Discord @negodev" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta property="og:title" content={`bencan.net: Anasayfa`} />
+        <meta property="og:title" content={`nego.rocks: Anasayfa`} />
         <meta property="og:description" content="Benim kişisel web sitem." />
-        <meta property="og:image" content="https://bencan.net/images/og-image.png" />
-        <meta property="og:url" content={`https://bencan.net`} />
+        <meta property="og:image" content="https://nego.rocks/images/og-image.png" />
+        <meta property="og:url" content={`https://nego.rocks`} />
         <meta property="og:type" content="website" />
         <meta property="og:locale" content="tr_TR" />
 
-        <meta name="twitter:title" content="bencan.net" />
+        <meta name="twitter:title" content="nego.rocks" />
         <meta name="twitter:description" content="Benim kişisel web sitem." />
-        <meta name="twitter:image" content="https://bencan.net/images/og-image.png" />
+        <meta name="twitter:image" content="https://nego.rocks/images/og-image.png" />
         <meta name="twitter:card" content="summary_large_image" />
 
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/favicon.ico" />
-        <link rel="canonical" href="https://bencan.net" />
+        <link rel="canonical" href="https://nego.rocks" />
       </Head>
       <div className="w-full h-full flex justify-center">
         <div className="w-full min-h-[100dvh] max-w-[700px] px-6 mb-12">
@@ -54,7 +54,7 @@ export default function Home({ discord_data, articles }) {
 
 export async function getServerSideProps() {
   try {
-    const response = await axios.get('https://api.lanyard.rest/v1/users/957840712404193290').then(res => res.data).catch(() => null);
+    const response = await axios.get('https://api.lanyard.rest/v1/users/682607343707488388').then(res => res.data).catch(() => null);
 
     const files = fs.readdirSync(`${process.cwd()}/public/articles`);
     const articles = files.map(filename => {
