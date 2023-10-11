e9669630a7897a0806c1f6871ce44493052b0e5f
Nihad <61694826+kardespro@users.noreply.github.com>
2023-10-11 10:45:14 +0400

    Upgrade TO Fast Product Build

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   images: {
     domains: ['i.scdn.co', 'cdn.discordapp.com', 'bencan.net', 'i.imgur.com'],
+  },
+  compress: true,
+  turboPack: {
+    enabled: true, // TurboPack'ı etkinleştirme veya devre dışı bırakma
+    polyfill: true, // TurboPack polyfill'ini etkinleştirme veya devre dışı bırakma
+    targets: ["es5", "module"], // Hedef platformları belirleme (örneğin, es5, es6, es7)
+    // Diğer TurboPack ayarlarını burada belirleyebilirsiniz
+  },
+  generateBuildId: async () => {
+    // Rastgele bir sayı veya benzersiz bir kimlik oluşturabilirsiniz
+    return "nego.rocks";
   }
 };
 
